8a51f9704a4f09a2caabb3c3b504900de235b22b
Dave Roberge <david.roberge@maine.edu>
2012-08-31 10:49:52 -0400

    Added atomfeed
    Change-Id: I13142b35659459d41213c1cb0f3e506fc7b348d4

---
+++ modules/openmrs/atomfeed/src/main/java/org/motechproject/openmrs/atomfeed/PollingProperties.java
@@ -0,0 +1,8 @@
+package org.motechproject.openmrs.atomfeed;
+
+public class PollingProperties {
+    private String pollingEnabled;
+    private String pollingType;
+    private String pollingStartTime;
+    private String pollingInterval;
+}
